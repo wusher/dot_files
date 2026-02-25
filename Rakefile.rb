1d9f77c48a9d0faea1b12335e073e7cf355b7d1a
wusher <149989+wusher@users.noreply.github.com>
2026-02-25 09:35:04 -0800

    rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,17 +1,38 @@
 
-@files = %w{ vimrc zshrc gemrc gitconfig tmux.conf}
+@files = %w[vimrc zshrc gemrc gitconfig tmux.conf]
+@dotfiles_dir = Dir.pwd
+@tmux_bin_files = %w[
+  tmux-mode-pill.sh
+  tmux-popup-menu.sh
+  tmux-right-status.py
+  tmux-workstate.py
+]
 
-task :install do 
+task :install do
   @files.each do |file_name|
     print "linking #{file_name}\n"
-    print `ln -s #{Dir.pwd}/#{file_name} ~/.#{file_name}`
+    print `ln -sfn #{@dotfiles_dir}/#{file_name} ~/.#{file_name}`
     print "\n"
-  end 
-end 
+  end
+end
 
-task :uninstall do 
+task :tmux do
+  print "linking tmux.conf\n"
+  print `ln -sfn #{@dotfiles_dir}/tmux.conf ~/.tmux.conf`
+
+  print "ensuring ~/bin exists\n"
+  print `mkdir -p ~/bin`
+
+  @tmux_bin_files.each do |file_name|
+    print "linking #{file_name}\n"
+    print `chmod +x #{@dotfiles_dir}/bin/#{file_name}`
+    print `ln -sfn #{@dotfiles_dir}/bin/#{file_name} ~/bin/#{file_name}`
+    print "\n"
+  end
+end
+
+task :uninstall do
   @files.each do |file_name|
     `rm ~/.#{file_name}`
-  end 
-
-end 
+  end
+end
